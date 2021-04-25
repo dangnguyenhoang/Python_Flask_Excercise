b922179f112ae980e72430faa224d180a7e35756
unknown <hoangdang.hm@gmail.com>
2021-04-26 01:10:33 +0700

    third version

--- app.py
+++ app.py
@@ -1,30 +1,63 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 import requests
-import sqlite3
+import sqlite3 as sql
 
-conn = sqlite3.connect('database.db')
-conn.execute('CREATE TABLE IF NOT EXISTS students (regnum TEXT, Name_ TEXT, email TEXT, hometown TEXT, birth TEXT, score TEXT)')
+conn = sql.connect('database.db')
+conn.execute('CREATE TABLE IF NOT EXISTS students (Regnum TEXT, Name_ TEXT, Email TEXT, Hometown TEXT, DoB TEXT, Score TEXT)')
+conn.close()
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb@'
 Bootstrap(app)
 
-OPEN_WEATHER_API_KEY= 'd6230d83d36a641f42b755ee87c62fe6'
-OPEN_WEATHER_URL = ('api.openweathermap.org/data/2.5/weather?zip={},uk&APPID=77d0e90e60b4eee00d6a9edf8997dd59')
+
 @app.route('/')
 
-# @app.route('/user/<name>')
-# def user(name):
-#     personal = f'<h1> Hello {name}, how are you</h1>'
-#     return personal
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         Regnum= request.form['Regnum']
+         Name_ = request.form['Name']
+         Email = request.form['Email']
+         Hometown = request.form['Hometown']
+         Score = request.form['Score']
+         Score = request.form['DoB']
+         
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (Regnum,Name_,Email,Hometown,DoB,Score) VALUES (?,?,?,?,?,?)",(Regnum,Name_,Email,Hometown,DoB,Score) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html")
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("List.html",rows = rows)
+   con.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
